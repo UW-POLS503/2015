6af2806af9f80e22659f0995d3906b4de34575f8
jrnold <jeffrey.arnold@gmail.com>
2015-04-01 12:14:00 -0700

    bugfix: bad string

--- web/conf.py
+++ web/conf.py
@@ -867,7 +867,7 @@ SEARCH_FORM = """
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
-BODY_END = "
+BODY_END = """
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -878,7 +878,7 @@ BODY_END = "
   ga('send', 'pageview');
 
 </script>
-"
+"""
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
