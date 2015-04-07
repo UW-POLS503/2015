1072726a97de1a040c534147fd299b61965f7b53
jrnold <jeffrey.arnold@gmail.com>
2015-04-07 15:16:10 -0700

    misc

--- lectures/init.R
+++ lectures/init.R
@@ -1,8 +1,22 @@
 # LECTURE_NUM needs to be set elsewhere
-knitr::opts_chunk$set(fig.height = 3, fig.width = 5,
-                      echo = FALSE, cache = TRUE, autodep = TRUE,
-                      message = FALSE, dev = "pdf",
+suppressPackageStartupMessages({
+    library("ggplot2")
+    library("dplyr")
+    library("stringr")
+    library("knitr")
+})
+
+opts_chunk$set(fig.height = 3, fig.width = 5,
+                      echo = FALSE,
+                      cache = TRUE,
+                      autodep = TRUE,
+                      message = FALSE,
+                      dev = "pdf",
                       error = FALSE,
                       fig.path = str_c('lecture-', LECTURE_NUM, '_files/'),
                       cache.path = str_c('lecture-', LECTURE_NUM, '_cache/'))
+pat <- knit_patterns[["get"]]()
+pat[["header.begin"]] <- "% HEADER HERE"
+knit_patterns[["set"]](pat)
+
 set.seed(57976496)
