b113fad4dfb6f268749a1185b1206beefb6d7c1f
jrnold <jeffrey.arnold@gmail.com>
2015-04-18 21:39:29 -0700

    code to run lintr on the labs R markdown

---
+++ labs/lintfiles.R
@@ -0,0 +1,36 @@
+library("knitr")
+library("lintr")
+lint_rmd <- function(filename, linters = default_linters) {
+	tmpfn <- tempfile()
+	on.exit(file.remove(tmpfn))
+	knitr::purl(filename, output = tmpfn, documentation = 2L, quiet = TRUE)
+	lints <- lintr::lint(tmpfn, linters = linters, cache = FALSE)
+	for (i in seq_along(lints)) {
+		lints[[i]][["filename"]] <- filename
+	}
+	lints
+}
+
+linters <- list(assignment_linter,
+								single_quotes_linter,
+								absolute_paths_linter,
+								no_tab_linter,
+								line_length_linter,
+								commas_linter,
+								infix_spaces_linter,
+								spaces_left_parentheses_linter,
+								spaces_inside_linter,
+								open_curly_linter,
+								object_multiple_dots_linter,
+								#object_camel_case_linter,
+								object_length_linter,
+								object_usage_linter,
+								trailing_whitespace_linter,
+								trailing_blank_lines_linter
+								)
+
+rmd_files <- dir(".", pattern = ".*\\.Rmd$")
+for (fn in rmd_files) {
+	print(fn)
+	print(lint_rmd(fn, linters = linters))
+}
