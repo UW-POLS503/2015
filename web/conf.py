200515557ee990f1014859788e9f375dad318dbf
jrnold <jeffrey.arnold@gmail.com>
2015-04-26 19:03:53 -0700

    don't use filter

--- web/conf.py
+++ web/conf.py
@@ -777,11 +777,12 @@ COMMENT_SYSTEM_ID = ""
 # Extra options to pass to the pandoc comand.
 # by default, it's empty, is a list of strings, for example
 # ['-F', 'pandoc-citeproc', '--bibliography=/Users/foo/references.bib']
-PANDOC_OPTIONS = ['-s',
+PANDOC_OPTIONS = ['--smart',
                   '-f', 'markdown+yaml_metadata_block+pipe_tables+header_attributes',
-                  '-F', 'pandoc-citeproc', 
-                  '--bibliography=../pols_503_sp15.bib',
-                  '--csl=../csl/chicago-syllabus.csl']
+                  # '-F', 'pandoc-citeproc', 
+                  # '--bibliography=../pols_503_sp15.bib',
+                  # '--csl=../csl/chicago-syllabus.csl'
+                  ]
 
 # Social buttons. This is sample code for AddThis (which was the default for a
 # long time). Insert anything you want here, or even make it empty.
