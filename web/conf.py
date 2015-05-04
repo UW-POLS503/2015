3c27121250376701b070f9538f2b15b9257e0a6f
jrnold <jeffrey.arnold@gmail.com>
2015-05-03 17:30:48 -0700

    misc

--- web/conf.py
+++ web/conf.py
@@ -735,7 +735,7 @@ COMMENT_SYSTEM_ID = ""
 # Generally, you want FUTURE_IS_NOW and DEPLOY_FUTURE to be the same value.
 # DEPLOY_FUTURE = False
 # If False, draft posts will not be deployed
-# DEPLOY_DRAFTS = True
+DEPLOY_DRAFTS = False
 
 # Allows scheduling of posts using the rule specified here (new_post -s)
 # Specify an iCal Recurrence Rule: http://www.kanzaki.com/docs/ical/rrule.html
