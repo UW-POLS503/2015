450ef108f8a24b97175bda9d24869b5fb87c9f7a
jrnold <jeffrey.arnold@gmail.com>
2015-05-19 15:28:35 -0700

    materials for Lecture on May 19

--- lectures/init.R
+++ lectures/init.R
@@ -1,5 +1,8 @@
 # LECTURE_NUM needs to be set elsewhere
 suppressPackageStartupMessages({
+    library("foreign")
+    library("MASS")
+    library("car")
     library("ggplot2")
     library("dplyr")
     library("stringr")
@@ -10,16 +13,18 @@ suppressPackageStartupMessages({
 })
 
 opts_chunk$set(fig.height = 3, fig.width = 5,
-                      echo = FALSE,
-                      cache = TRUE,
-                      autodep = TRUE,
-                      message = FALSE,
-                      dev = "pdf",
-                      error = FALSE,
-                      fig.path = str_c('lecture-', LECTURE_NUM, '_files/'),
-                      cache.path = str_c('lecture-', LECTURE_NUM, '_cache/'))
+               echo = FALSE,
+               cache = TRUE,
+               autodep = TRUE,
+               message = FALSE,
+               dev = "pdf",
+               error = FALSE,
+               fig.path = str_c("lecture-", LECTURE_NUM, "_files/"),
+               cache.path = str_c("lecture-", LECTURE_NUM, "_cache/"),
+               size = "footnotesize")
+                   
 pat <- knit_patterns[["get"]]()
-pat[["header.begin"]] <- "% HEADER HERE"
+pat[["header.begin"]] <- "%%%INSERTHEADERHERE"
 knit_patterns[["set"]](pat)
 
 set.seed(5797646)
